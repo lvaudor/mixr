64852abf03a1d89156aeeaf01fc56637b8c0053a
Lise Vaudor <lise.vaudor@ens-lyon.fr>
2019-01-09 15:30:19 +0100

    add tidy_geocode doc

--- R/tidy_geocode.R
+++ R/tidy_geocode.R
@@ -1,31 +1,27 @@
-#' Returns a tibble with specificities according to two crossed categories.
+#' Returns a tibble that includes a variable describing locations, with added information corresponding to the Data Science Toolkit geocoding tool.
 #' @param data a tibble
-#' @param cat1 a factor corresponding to words or lemmas
-#' @param cat2 a category
-#' @param criterion one of "all" (default), "top_n" or "min_spec": should the information displayed be filtered by top specificities (top_n) or according to a minimum value of specificity (min_spec).
-#' @param top_n in case criterion=='top_n', how many items by category should be kept (defaults to 50)
-#' @param min_spec in case criterion=='min_spec', which is the minimum specificity for an item to be kept (defaults to 2)
-#' @return tibble with additional columns cat1, cat2, spec
+#' @param location the variable to geocode
+#' @return tibble with additional columns lat, lng, country, locality, area
 #' @export
 #' @examples
 #' df <-tibble(loc=c("Lyon, France",
 #'                   "22 place du Général de Gaulle, Paris",
 #'                   "la Guillotière, Lyon",
 #'                   "Europe",
-#'                   "Tucson, AZ"))
+#'                   "Tucson, AZ",
+#'                   "Rio Grande do Sul"))
 #' tidy_geocode(df,loc)
-tidy_geocode=function(df,location){
+tidy_geocode=function(data,location){
   qlocation=enquo(location)
   geocode_dsk_all=function(loc){
     result=ggmap::geocode(loc, source="dsk", output="all")
     return(result)}
-  stringslocations=select(df,!!qlocation) %>%
+  stringslocations=select(data,!!qlocation) %>%
     pull(1)
   locations= stringslocations%>%
     map(safely(geocode_dsk_all)) %>%
     map("result") %>%
     map("results")
-  print(locations)
   coords=locations %>%
     map(1) %>%
     map("geometry") %>%
@@ -37,7 +33,6 @@ tidy_geocode=function(df,location){
     map(function(x){select(x,lat,lng)}) %>%
     map(function(x){if(is.null(x)) x=c(lat=NA,lng=NA) else x=x}) %>%
     bind_rows()
-
   comp=locations %>%
     map(1) %>%
     map("address_components") %>%
@@ -70,8 +65,8 @@ tidy_geocode=function(df,location){
                 country,
                 locality,
                 area)
-  colnames(coords)[1]=colnames(select(df,!!qlocation))
-  df=left_join(df,coords,by=colnames(coords)[1])
-  return(df)
+  colnames(coords)[1]=colnames(select(data,!!qlocation))
+  data=left_join(data,coords,by=colnames(coords)[1])
+  return(data)
 }
 
