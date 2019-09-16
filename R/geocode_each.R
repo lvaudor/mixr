798835a37b754c6e6527b90b784e8906043321cd
Lise Vaudor <lise.vaudor@ens-lyon.fr>
2019-09-16 17:08:06 +0200

    if no result from API

--- R/geocode_each.R
+++ R/geocode_each.R
@@ -7,15 +7,21 @@
 #' register_google(key="[your key]")
 #' geocode_each("la Guillotière, Lyon")
 geocode_each=function(stringlocation,info=c("latlng")){
-  url_base <- "https://maps.googleapis.com/maps/api/geocode/json?"
-  if(is.na(stringlocation)){
-    #latlng
-    latlng=tibble(lat=NA,lng=NA)
-    #other info
+  make_empty_result=function(info){
     result=rep(NA,length(info))
     names(result)=info
     result=result %>%
       bind_rows()
+    if("latlng" %in% info){
+      result=bind_cols(tibble(lat=NA,lng=NA),
+                      result) %>%
+        select(-latlng)
+    }
+    return(result)
+  }
+  url_base <- "https://maps.googleapis.com/maps/api/geocode/json?"
+  if(is.na(stringlocation)){
+    result=make_empty_result(info)
   }
   if(!is.na(stringlocation)){
       # API response
@@ -31,43 +37,48 @@ geocode_each=function(stringlocation,info=c("latlng")){
                                 collapse = "&")
       url <- str_c(url_base, url_query_inline)
       rep=httr::GET(url)
-      repcontent=rep %>%
-        httr::content() %>%
-        .$results
-      # latlng
-      latlng=repcontent %>%
-        .[[1]] %>%
-        .$geometry %>%
-        .$location %>%
-        as_tibble()
-      # address components
-      address_components=repcontent %>%
-        .[[1]] %>%
-        .$address_components %>%
-        map_df(as_tibble) %>%
-        mutate(types=unlist(types)) %>%
-        filter(types!="political")
-      formatted_address=repcontent %>%
-        .[[1]] %>%
-        .$formatted_address
-      address_components=bind_rows(address_components,
-                                   tibble(long_name=formatted_address,
-                                          short_name=NA,
-                                          types="formatted_address"),
-                                   tibble(long_name=address_components %>% filter(types=="country") %>% pull(short_name),
-                                          short_name=NA,
-                                          types="country_code")) %>%
-        select(name=long_name,
-               types=types)
-      result=address_components %>%
-        right_join(tibble(types=info), by="types") %>%
-        select(types,name) %>%
-        deframe() %>%
-        bind_rows()
-  }
-  if("latlng" %in% info){
-    result=bind_cols(latlng,result) %>%
-      select(-latlng)
+      if(is.null(rep$results)){
+        result=make_empty_result(info)
+      }
+      if(!is.null(rep$results)){
+          repcontent=rep %>%
+            httr::content() %>%
+            .$results
+          # latlng
+          latlng=repcontent %>%
+            .[[1]] %>%
+            .$geometry %>%
+            .$location %>%
+            as_tibble()
+          # address components
+          address_components=repcontent %>%
+            .[[1]] %>%
+            .$address_components %>%
+            map_df(as_tibble) %>%
+            mutate(types=unlist(types)) %>%
+            filter(types!="political")
+          formatted_address=repcontent %>%
+            .[[1]] %>%
+            .$formatted_address
+          address_components=bind_rows(address_components,
+                                       tibble(long_name=formatted_address,
+                                              short_name=NA,
+                                              types="formatted_address"),
+                                       tibble(long_name=address_components %>% filter(types=="country") %>% pull(short_name),
+                                              short_name=NA,
+                                              types="country_code")) %>%
+            select(name=long_name,
+                   types=types)
+          result=address_components %>%
+            right_join(tibble(types=info), by="types") %>%
+            select(types,name) %>%
+            deframe() %>%
+            bind_rows()
+          if("latlng" %in% info){
+            result=bind_cols(latlng,result) %>%
+              select(-latlng)
+          }
+      }
   }
   return(result)
 }
