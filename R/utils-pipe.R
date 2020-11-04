7edb2c54b95582f117cd0e9f2a3bcb94224a1924
lvaudor <lise.vaudor@ens-lyon.fr>
2020-11-04 15:07:01 +0100

    add make_empty_result() to utils

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -1,3 +1,16 @@
+
+#' Make empty line
+#' @param info the column names
+#' @example make_empty_result(info=c("lat","lng"))
+make_empty_result=function(info){
+  result=rep(NA,length(info))
+  names(result)=info
+  result=result %>%
+    dplyr::bind_rows()
+  return(result)
+}
+
+
 #' Pipe operator
 #'
 #' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
